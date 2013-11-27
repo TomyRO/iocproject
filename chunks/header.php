5f6cb2c174819fdd62ef2ff4dcd455f103513e76
Tomy <alex.dan.tomescu@gmail.com>
2013-11-27 22:22:38 +0200

    fixed menu

--- chunks/header.php
+++ chunks/header.php
@@ -4,13 +4,13 @@
 			<div id="logo">
 				<h1><a href="#">Logo</a></h1>
 			</div>
+            <div id="searchform">
+                <form action="#" method="get">
+                    <input name="q" value="Search keywords" />
+                    <input type="submit" class="icon icon-search" value="Search" />
+                </form>
+            </div>
 			<div id="menu">
-				<div id="searchform">
-					<form action="#" method="get">
-						<input name="q" value="Search keywords" />
-						<input type="submit" class="icon icon-search" value="Search" />
-					</form>
-				</div>
 				<ul>
 					<li><a href="#" accesskey="1" title="">Categories</a></li>
 					<li><a href="#" accesskey="2" title="">Articles</a></li>
