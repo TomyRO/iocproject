e6408059b859bc8562126a72ef662ebd9ca1d8d8
Tomy <alex.dan.tomescu@gmail.com>
2013-11-28 00:45:43 +0200

    added unfinised article and comments sections

--- controller.php
+++ controller.php
@@ -58,7 +58,7 @@ $arrCategories = array (
 		"title" => "Networking",
 		"icon" => "fa-sitemap",
 	),
-);	
+);
 
 /* LOGIN */
 if (isset($_GET["page"]) && $_GET["page"] == "login" && isset($_POST["btnLogin"]))
@@ -67,7 +67,7 @@ if (isset($_GET["page"]) && $_GET["page"] == "login" && isset($_POST["btnLogin"]
 	$strPasswordEncrypted = md5($_POST["password"]);
 	$login = array_reduce(
 		$arrUsers,
-		function ($result, $item) 
+		function ($result, $item)
 		{
 			global $strEmail, $strPasswordEncrypted;
 			if ($item["email"]==$strEmail && $item["password"]==$strPasswordEncrypted)
