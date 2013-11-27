fdeac944ed36b100657b84211fc3710a23492006
Tomy <alex.dan.tomescu@gmail.com>
2013-11-27 23:39:54 +0200

    fixed category page and sidebar

---
+++ chunks/category.php
@@ -0,0 +1,27 @@
+<ul id="gallery">
+    <li class="tutorial-item">
+        <a href="#"><div class="tutorial-image"></div></a>
+        <div class="tutorial-descript">
+            <a href="#"><h2> XML Parsing </h2></a>
+            <span class="tutorial-author">Author: John Doe</span>
+            <p>Lorem ipsum doclor sit amet, consectetur adipiscing elit. Mauris pellentesque, metus vel sagittis tincidunt, massa tortor ullamcorper eros, eu dictum eros urna ac turpis. Aliquam sed tellus lorem. Pellentesque condimentum quam sodales sapien consequat fermentum. eleifend aliquam.</p>
+        </div>
+    </li>
+    <li class="tutorial-item">
+        <a href="#"><div class="tutorial-image"></div></a>
+        <div class="tutorial-descript">
+            <a href="#"><h2> XML Parsing </h2></a>
+            <span class="tutorial-author">Author: John Doe</span>
+            <p>Lorem ipsum doclor sit amet, consectetur adipiscing elit. Mauris pellentesque, metus vel sagittis tincidunt, massa tortor ullamcorper eros, eu dictum eros urna ac turpis. Aliquam sed tellus lorem. Pellentesque condimentum quam sodales sapien consequat fermentum. eleifend aliquam.</p>
+        </div>
+    </li>
+    <li class="tutorial-item">
+        <a href="#"><div class="tutorial-image"></div></a>
+        <div class="tutorial-descript">
+            <a href="#"><h2> XML Parsing </h2></a>
+            <span class="tutorial-author">Author: John Doe</span>
+            <p>Lorem ipsum doclor sit amet, consectetur adipiscing elit. Mauris pellentesque, metus vel sagittis tincidunt, massa tortor ullamcorper eros, eu dictum eros urna ac turpis. Aliquam sed tellus lorem. Pellentesque condimentum quam sodales sapien consequat fermentum. eleifend aliquam.</p>
+        </div>
+    </li>
+</ul>
+<?php require_once("sidebar.php") ?>
---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div id="sidebar">
+    <span id="top-rated">Top Rated</span>
+    <ul id="top-rated-list">
+        <li class="top-tutorial"><a href="#">Tutorial 1</a></li>
+        <li class="top-tutorial"><a href="#">Tutorial 2</a></li>
+        <li class="top-tutorial"><a href="#">Tutorial 3</a></li>
+        <li class="top-tutorial"><a href="#">Tutorial 4</a></li>
+        <li class="top-tutorial"><a href="#">Tutorial 5</a></li>
+        <li class="top-tutorial"><a href="#">Tutorial 6</a></li>
+    </ul>
+</div>
